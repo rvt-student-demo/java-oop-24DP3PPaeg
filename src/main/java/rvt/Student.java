c2aa319a29da7f019743ba9d5f50da6eaad8fe81
24DP3PPaeg <A240737PP@rvt.lv>
2026-05-12 11:26:56 +0300

    Student java (kods nemainās, nezinu kāpēc nav commitots ātrāk)

--- src/main/java/rvt/Student.java
+++ src/main/java/rvt/Student.java
@@ -2,18 +2,21 @@ package rvt;
 
 public class Student extends Person {
     private int credits;
-    public Student(String name, String address, int credits){
+
+    public Student(String name, String address, int credits) {
         super(name, address);
         this.credits = credits;
     }
 
-    public int credits(){
+    public int credits() {
         return this.credits;
     }
-    public int study(){
-        return credits ++;
+
+    public int study() {
+        return credits++;
     }
-    public String toString(){
-        return super.toString() + "\n " + "Study credits"+ credits;
+
+    public String toString() {
+        return super.toString() + "\n " + "Study credits" + credits;
     }
 }
